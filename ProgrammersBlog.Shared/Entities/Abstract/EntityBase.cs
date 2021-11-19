19614889af43bbf53c9521f098ef31b9266051cc
elifelse <elifgsavukcam@gmail.com>
2021-11-19 12:22:49 +0300

    architecture layer, the necessary abstract interface for the repository pattern was created. Entities are created in the Entities layer.

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now; //override CreatedDate = DateTime(2021/11/21)
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual string ModifiedByName { get; set; } = "Admin";
+        public string Note { get; set; }
+    }
+}
