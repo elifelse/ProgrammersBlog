ca13599610287bf8a9e80b51cc52903da955575d
elifelse <elifgsavukcam@gmail.com>
2021-11-23 14:32:12 +0300

    Result interface and class created with status

---
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,12 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
---
+++ ProgrammersBlog.Shared/Utilities/Results/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammersBlog.Shared.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        Success = 0,
+        Error = 1,
+        Warning = 2,
+        Info = 3
+    }
+}
