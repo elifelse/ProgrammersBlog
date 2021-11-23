36e8e0ff14304533b57bf1b0e6dcf68e161dc273
elifelse <elifgsavukcam@gmail.com>
2021-11-24 00:35:09 +0300

    Startup.cs Configuration settings

--- ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -4,11 +4,6 @@ using ProgrammersBlog.Data.Concrete;
 using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
 using ProgrammersBlog.Services.Abstract;
 using ProgrammersBlog.Services.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Services.Extensions
 {
